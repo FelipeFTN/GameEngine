d6536d952988371f3fbb25026bc170db2dca70c9
FelipeFTN <FelipeFTN@protonmail.com>
2024-12-21 20:26:42 -0300

    Implementing some Physics & Body

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #include "../include/glad/glad.h"
 
 #include "engine/global.h"
+#include "engine/physics.h"
 #include "engine/config.h"
 #include "engine/input.h"
 #include "engine/time.h"
@@ -30,6 +32,19 @@ int main(int argc, char *argv[]) {
     time_init(60);
     config_init();
     render_init();
+    physics_init();
+
+    u32 body_count = 100;
+
+    for (u32 i = 0; i < body_count; i++) {
+        usize body_index = physics_body_create(
+            (vec2){ rand() % (i32)global.render.width, rand() % (i32)global.render.height },
+            (vec2){ rand() % 100, rand() % 100 }
+        );
+        Body *body = physics_body_get(body_index);
+        body->acceleration[0] = rand() % 200 - 100;
+        body->acceleration[1] = rand() % 200 - 100;
+    }
 
     pos[0] = global.render.width * 0.5f; // X
     pos[1] = global.render.height * 0.5f; // Y
@@ -51,6 +66,7 @@ int main(int argc, char *argv[]) {
 
         input_update();
         input_handle();
+        physics_update();
 
         render_begin();
 
@@ -58,6 +74,26 @@ int main(int argc, char *argv[]) {
             (vec2){50, 50},
             (vec4){0.0f, 1.0f, 0.0f, 1.0f}
         );
+
+        for (u32 i = 0; i < body_count; i++) {
+            Body *body = physics_body_get(i);
+            render_quad(body->aabb.position, body->aabb.half_size, (vec4){1.0f, 0.0f, 0.0f, 1.0f});
+
+            if (body->aabb.position[0] < 0 || body->aabb.position[0] > global.render.width)
+                body->velocity[0] *= -1;
+
+            if (body->aabb.position[1] < 0 || body->aabb.position[1] > global.render.height)
+                body->velocity[1] *= -1;
+
+            if (body->velocity[0] > 500)
+                body->velocity[0] = 500;
+            if (body->velocity[0] < -500)
+                body->velocity[0] = -500;
+            if (body->velocity[1] > 500)
+                body->velocity[1] = 500;
+            if (body->velocity[1] < -500)
+                body->velocity[1] = -500;
+        }
 
         render_end();
         time_update_late();
